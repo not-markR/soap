39db0c209e090ff2e062fd6f563697be6fbd4e87
Mark Ryan B. Liscabo <119417193+not-markR@users.noreply.github.com>
2023-03-08 15:48:10 +0800

    first commit

---
+++ papao/toggle.js
@@ -0,0 +1,10 @@
+
+function toggle() {
+  document.getElementById("linklist").style.display = "flex";
+  document.getElementById("times").style.visibility = "visible";
+  document.getElementById("times").style.display = "block";
+}
+function toggleClose() {
+  document.getElementById("linklist").style.display = "none";
+  document.getElementById("times").style.visibility = "hidden";
+}
